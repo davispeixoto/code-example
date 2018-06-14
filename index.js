2da21fbfd4d6e6468a0a6d85b12065fdc73150be
davegregg <davegregg@gmail.com>
2018-06-14 13:01:00 -0400

    init

---
+++ index.js
@@ -0,0 +1,49 @@
+function Animal (name, health = 90, species, class, parentElement) {
+  this.name = name
+  this.species = species
+  this.class = class
+  this.parentElement = parentElement || document.body
+
+  this.createElement()
+  this.parentElement.appendChild(this.element)
+
+  this.element.addEventListener("click", this.feed)
+}
+
+Animal.prototype.createElement = function () {
+  this.element = document.createElement("div")
+  this.element.classList.add(this.class)
+  this.element.dataset.health = this.health
+
+  return this
+}
+
+Animal.prototype = {
+
+  constructor: Object,
+
+  get element () {
+    if (this.element) return element
+
+    this.element = document.createElement("div")
+    this.element.classList.add(this.class)
+    this.element.dataset.health = this.health
+    
+    return this.element
+  },
+  
+  set element (element) {
+    return (this.element = element)
+  },
+
+  feed: function () {
+    this.health = this.element.dataset.health + 10
+
+    if (this.health > 100) this.health = 100
+
+    return this.health
+  },
+
+}
+
+const frankieThePug = new Animal("Frankie the Pug", 60, "dog", "animal", document.getElementById("dogKennel"))
